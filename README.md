d330cc3e62881e40ab5a11eb9ff8c0e1dbc5626c
wildan14ar <wildan1423ar@gmail.com>
2024-11-23 21:31:44 +0700

    feat: first commit frontend

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # pathgrow-capstone
+
+### Deploy Backend Server
+
+```
+cd backend
+npm i
+```
+
+###  Deploy Frontend Client
+
+```
+cd client
+npm i
+npm run dev
+```
---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-full flex justify-center text-center">
+      Hello, Pathgrow.
+    </div>
+  )
+}
+
+export default App
