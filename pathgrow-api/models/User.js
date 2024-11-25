a24aefa4c03b4b69aa697df4a0c4193da7eb56c0
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-11-25 16:48:00 +0800

    model

---
+++ pathgrow-api/models/User.js
@@ -0,0 +1,18 @@
+const db = require('../db');
+
+class User {
+  static async findByEmail(email) {
+    const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    return result.rows[0];
+  }
+
+  static async create(name, email, hashedPassword, role = 'student') {
+    const result = await db.query(
+      'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, email, hashedPassword, role]
+    );
+    return result.rows[0];
+  }
+}
+
+module.exports = User;
