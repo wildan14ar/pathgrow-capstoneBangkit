c65196459f05140816a58977304745eb6b1a038d
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-12 21:25:34 +0800

    init

--- pathgrow-api/utils/token.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = { generateToken };
