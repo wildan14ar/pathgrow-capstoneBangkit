379c2edf0310c37ed6ed5a8cd428a5e0960522bd
wildan14ar <wildan1423ar@gmail.com>
2024-12-12 12:09:09 +0700

    feat: update ml model

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import QuizPage from "./pages/QuizPage";
+import ProtectedRoute from "./middleware/ProtectedRoute";
 
 const AppContent = () => {
   const location = useLocation(); // Get current location
@@ -16,7 +17,19 @@ const AppContent = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <div>Dashboard</div>
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/quiz" element={
+          <ProtectedRoute>
+            <QuizPage />
+          </ProtectedRoute>
+        } />
       </Routes>
     </>
   );
