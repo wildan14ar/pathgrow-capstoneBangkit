9ab55eef8f305168c1dde3b6d2b9a9f9b7ac3aac
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-16 21:42:57 +0800

    chore: update connection for MySQL

--- pathgrow-api/src/config/mysql.js
+++ pathgrow-api/src/config/mysql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Kepiting_28!',
-  database: 'db_pathgrow'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
 module.exports = dbPool.promise();
