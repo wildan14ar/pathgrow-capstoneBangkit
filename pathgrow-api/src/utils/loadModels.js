1cdb6a28f4ed14b079eb80802c1db6143f187c0f
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-16 21:50:28 +0800

    feat: move folders & files to src/

---
+++ pathgrow-api/src/utils/loadModels.js
@@ -0,0 +1,15 @@
+const tf = require('@tensorflow/tfjs-node');
+
+// Fungsi untuk memuat model TensorFlow
+const loadCareerModel = async () => {
+    try {
+        const model = await tf.loadModel('file://career_model/model.json');
+        console.log('Career model successfully loaded.');
+        return model;
+    } catch (error) {
+        console.error('Failed to load career model:', error);
+        throw new Error('Could not load career model.');
+    }
+};
+
+module.exports = { loadCareerModel };
