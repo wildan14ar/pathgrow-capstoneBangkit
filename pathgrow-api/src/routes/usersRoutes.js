dd85a4c3447c62b78c2bdb007ade26fa198a0bc4
Gerent Yoel Mamahani <gerentyoel02@gmail.com>
2024-12-16 21:47:30 +0800

    endpoints

--- pathgrow-api/src/routes/usersRoutes.js
+++ pathgrow-api/src/routes/usersRoutes.js
@@ -3,19 +3,8 @@ const express = require('express');
 const userController = require('../controller/usersController');
 const router = express.Router();
 
-// CREATE - POST method
-router.post('/', userController.createNewUser);
-
-
-// READ - GET method
-router.get('/', userController.getAllUsers);
-
-// UPDATE - PATCH method
-router.patch('/:userID', userController.updateUser);
-
-// DELETE - DELETE method
-router.delete('/:userID', userController.deleteUser);
-
-
+router.get('/', userController.getAllUsers);            // GET all users
+router.patch('/:userID', userController.updateUser);    // UPDATE user
+router.delete('/:userID', userController.deleteUser);   // DELETE user
 
 module.exports = router;
